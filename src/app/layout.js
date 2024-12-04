f0e83cc8890f85ccd6c8177f85999f66e47cb86e
Priyanka <priyanka@version-next.com>
2024-12-04 10:25:43 +0530

    changes done

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,8 +19,6 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         />
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"
-        />
       </head>
       <body>
         <CurrencyProvider>
